011da1177c63a309874b7ccf695e6005c6815b9e
Eric Williams <53901011+ewilliams0305@users.noreply.github.com>
2023-11-07 21:47:02 -0500

    Update RequestResultExtensions.cs

---
+++ source/Result/RequestResultExtensions.cs
@@ -0,0 +1,34 @@
+namespace HttpClientBuilder
+{
+   public static class ResponseResultExtensions
+   {
+        /// <summary>
+    /// Checks the result and ensures its a successful result before invoking a predicate.
+    /// If the predicate returns true, a new successful result is returned.
+    /// If the predicate fails, a default error is returned.
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="result"></param>
+    /// <param name="predicate"></param>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    public static async Task<IResponseResult<TValue>> EnsureAsync<TValue>(
+        this Task<IResponseResult<TValue>> result,
+        Func<TValue, Task<bool>> predicate,
+        Func<TValue, Exception> error)
+    {
+        var resultFromTask = await result;
+
+        if (!resultFromTask.Success) return 
+            result.Exception!;
+
+        if (await predicate(resultFromTask.Value!))
+        {
+            return new IResponseResult<TValue>(resultFromTask.Value!);
+        }
+
+        return error(resultFromTask.Value!);
+    }
+   }
+
+}
